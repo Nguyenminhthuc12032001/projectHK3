ca1121fae70f4686ed7743e402f383a4623fb881
Dante97vn <106880465+Dante97vn@users.noreply.github.com>
2025-11-08 20:50:52 +0700

    Dat delete frontend next

--- projecthk3_frontend/app/(employee)/employee/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div className="text-center mt-10">
-      <h1 className="text-4xl font-bold text-blue-700">Welcome to My Website!</h1>
-      <p className="mt-4 text-gray-600">This is EmployeePage.</p>
-    </div>
-  );
-}
--- projecthk3_frontend/app/component/Header.tsx
+++
@@ -1,3 +0,0 @@
- export default function Header() {
-  return <h1>This is Header </h1>
-}
