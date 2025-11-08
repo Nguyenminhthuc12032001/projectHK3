d8e37bc3d7a944528d777fdc752d60dfb7534664
Dante97vn <106880465+Dante97vn@users.noreply.github.com>
2025-11-08 18:54:23 +0700

    Dat commit navbar

--- projecthk3_frontend/app/component/PublicNavbar.tsx
+++ projecthk3_frontend/app/component/PublicNavbar.tsx
@@ -12,13 +12,16 @@ import {
   NavbarLink,
   NavbarToggle,
 } from "flowbite-react";
-
+const navLinkClass =
+  "!text-white hover:!text-cyan-300 md:hover:!text-cyan-300";
+const navLinkActiveClass =
+  "!text-cyan-300 font-semibold hover:!text-cyan-100 md:hover:!text-cyan-100";
 export default function PublicNavbar() {
   return (
-    <Navbar className="bg-blue-900 dark:bg-blue-900"  >
+    <Navbar className="bg-blue-900  text-white dark:bg-blue-900 "  >
       <NavbarBrand as ={Link} href="/">
         <img src="/logo-1.png" className="mr-4 h-8 sm:h-12" alt="Flowbite React Logo" />
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Medical Care</span>
+        <span className=" self-center whitespace-nowrap text-xl font-semibold dark:text-white">Medical Care</span>
       </NavbarBrand>
       <div className="flex md:order-2">
         <Dropdown
@@ -40,14 +43,14 @@ export default function PublicNavbar() {
         </Dropdown>
         <NavbarToggle />
       </div>
-      <NavbarCollapse>
-        <NavbarLink as={Link} href="#" active>
+      <NavbarCollapse >
+        <NavbarLink as={Link} href="#" active className={navLinkClass} >
           Home
         </NavbarLink>
-        <NavbarLink as={Link} href="/about">About</NavbarLink>
-        <NavbarLink as={Link} href="/contact">Contact</NavbarLink>
-        <NavbarLink as={Link} href="/feedback">Feedback</NavbarLink>
-        <NavbarLink  as={Link} href="/login">Login/Register</NavbarLink>
+        <NavbarLink className={navLinkClass} as={Link} href="/about">About</NavbarLink>
+        <NavbarLink className={navLinkClass} as={Link} href="/contact">Contact</NavbarLink>
+        <NavbarLink className={navLinkClass} as={Link} href="/feedback">Feedback</NavbarLink>
+        <NavbarLink className={navLinkClass} as={Link} href="/login">Login/Register</NavbarLink>
       </NavbarCollapse>
     </Navbar>
   );
