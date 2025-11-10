18124d8dd0c4fdafc5c6d05dad8e7a814790ebba
Dante97vn <106880465+Dante97vn@users.noreply.github.com>
2025-11-10 21:25:39 +0700

    Dat update change

--- Project3_Frontend/src/App.jsx
+++ Project3_Frontend/src/App.jsx
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PublicLayout from "./layout/PublicLayout";
-import AdminLayout from "./layout/AdminLayout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        {/* Layout cho người dùng */}
-        <Route element={<PublicLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-
-        {/* Layout cho admin */}
-        <Route path="/admin" element={<AdminLayout />}>
-          <Route index element={<Dashboard />} />
-        </Route>
-
-        {/* 404 page */}
-        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
-export default App;
-=======
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
@@ -40,4 +8,4 @@ export default function App() {
     </BrowserRouter>
   );
 }
->>>>>>> a36765052ec7b29b88675d49e1e4a831c701230f
+//
