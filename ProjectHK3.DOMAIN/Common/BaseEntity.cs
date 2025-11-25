f87630931759c59e34ed8400e9e5cf4ec06243dc
Nguyenminhthuc12032001 <nguyenminhthuc12032001@gmail.com>
2025-11-25 20:30:36 +0700

    Thuc Update code

--- ProjectHK3.DOMAIN/Common/BaseEntity.cs
+++ ProjectHK3.DOMAIN/Common/BaseEntity.cs
@@ -3,9 +3,10 @@
     internal class BaseEntity : IEntity
     {
         public Guid Id { get; set; }
-
         public DateTime CreatedAt { get; set; }
+        public DateTime CreatedBy { get; set; }
         public DateTime UpdatedAt { get; set; }
+        public DateTime UpdatedBy { get; set; }
         public DateTime DeletedAt { get; set; }
     }
 }
