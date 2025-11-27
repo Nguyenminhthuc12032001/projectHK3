2f4c0b939dc471f151890d508136d5071e6da005
Nguyenminhthuc12032001 <nguyenminhthuc12032001@gmail.com>
2025-11-27 22:11:35 +0700

    Thuc update code

---
+++ ProjectHK3.Infrastructure/Repositories/Policy Enrollment & Claims/PolicyApprovalDetailRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHK3.Infrastructure.Repositories.Policy_Enrollment___Claims
+{
+    internal class PolicyApprovalDetailRepo
+    {
+    }
+}
