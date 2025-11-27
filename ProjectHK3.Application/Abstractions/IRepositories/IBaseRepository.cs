d2afe5c43b3eba144f2d8028ad5a98548131d207
Nguyenminhthuc12032001 <nguyenminhthuc12032001@gmail.com>
2025-11-27 20:42:39 +0700

    Thuc Update code

--- ProjectHK3.Application/Abstractions/IRepositories/IBaseRepository.cs
+++ ProjectHK3.Application/Abstractions/IRepositories/IBaseRepository.cs
@@ -3,9 +3,9 @@
     public interface IBaseRepository<T> where T : class
     {
         Task<T?> AddOneAsync(T entity);
-        Task<T?> GetOneAsync(Guid id);
+        Task<T?> GetOneAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         Task<T?> UpdateOneAsync(T entity);
-        Task<bool> DeleteOneAsync(Guid id);
+        Task<bool> DeleteOneAsync(int id);
     }
 }
--- ProjectHK3.Infrastructure/Repositories/Authentication/AdminLoginRepo.cs
+++ ProjectHK3.Infrastructure/Repositories/Authentication/AdminLoginRepo.cs
@@ -1,12 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectHK3.Application.Abstractions.IRepositories.Authentication;
+using ProjectHK3.Domain.Emtitys;
+using ProjectHK3.Infrastructure.Persistence;
 
 namespace ProjectHK3.Infrastructure.Repositories.Authentication
 {
-    internal class AdminLoginRepo
+    public class AdminLoginRepo(ApplicationDbContext context) : IAdminLoginRepo
     {
+        readonly ApplicationDbContext _context = context;
+        public async Task<AdminLogin?> AddOneAsync(AdminLogin entity)
+        {
+            await _context.AdminLogin.AddAsync(entity);
+            return entity;
+        }
+
+        public async Task<bool> DeleteOneAsync(int id)
+        {
+            var match = await _context.AdminLogin.FindAsync(id);
+            if (match == null) return false;
+
+            _context.AdminLogin.Remove(match);
+            return true;
+        }
+
+        public async Task<IEnumerable<AdminLogin>> GetAllAsync()
+        {
+            return await _context.AdminLogin.ToListAsync();
+        }
+
+        public async Task<AdminLogin?> GetOneAsync(int id)
+        {
+            return await _context.AdminLogin.FindAsync(id);
+        }
+
+        public async Task<AdminLogin?> UpdateOneAsync(AdminLogin entity)
+        {
+            var match = await _context.AdminLogin.FindAsync(entity.Id);
+            if (match == null) return null;
+            _context.AdminLogin.Update(entity);
+            return match;
+        }
     }
 }
